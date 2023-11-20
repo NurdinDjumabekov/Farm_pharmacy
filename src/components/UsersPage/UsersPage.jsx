e042622a0d933fc4cc64806e910b9e2e23373233
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-11-20 15:06:47 +0600

    небольшой рефакторинг кода!

--- src/components/UsersPage/UsersPage.jsx
+++ src/components/UsersPage/UsersPage.jsx
@@ -1,16 +1,14 @@
 import UserCard from '../UserCard/UserCard';
-import cls from './studentsPage.module.css'
+import cls from './studentsPage.module.css';
 
-const UsersPage = ({ users }) =>
-{
-
-    return (
-        <main className={cls.studentsWrapper}>
-            {
-                users.map(user => <UserCard key={user.id} userData={user} />) || <h3 style={{ textAlign: 'center', marginTop: '8px' }}>Список Пользователей пуст</h3>
-            }
-        </main>
-    )
+const UsersPage = ({ users }) => {
+  return (
+    <main className={cls.studentsWrapper}>
+      {users.map((user) => <UserCard key={user.id} userData={user} />) || (
+        <h3 className={cls.noneData}>Список Пользователей пуст</h3>
+      )}
+    </main>
+  );
 };
 
 export default UsersPage;
